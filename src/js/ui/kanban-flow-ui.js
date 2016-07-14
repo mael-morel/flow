79514b4f0b7e3522c10a96cfbb30077cb47be4ed
Mateusz Gajdizk <mgajdzik@gmail.com>
2016-07-14 10:22:49 +0200

    cache separated from ui god file

--- src/js/ui/kanban-flow-ui.js
+++ src/js/ui/kanban-flow-ui.js
@@ -633,30 +633,6 @@ function GUI(hookSelectorParam, simulation, configuration) {
 	}
 }
 
-var cache = new Cache();
-var hookSelector = "";
-function $$(selector, useCache) {
-	if (useCache == undefined || useCache) {
-		return cache.get(hookSelector +" " + selector);
-	}
-	return $(hookSelector +" " + selector);
-}
-
-function Cache() {
-	this.cache = {};
-	this.get = function(query) {
-		var value = this.cache[query];
-		if (value) return value;
-		var jquery = $(query);
-		this.cache[query] = jquery;
-		return jquery;
-	}
-	
-	this.put = function(query, value) {
-		this.cache[query] = value;
-	}
-}
-
 $.fn.slideFadeToggle = function(easing, callback) {
   return this.animate({ opacity: 'toggle', height: 'toggle' }, 'fast', easing, callback);
 };
