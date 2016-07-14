1abfde06ff205d12c4d8ddaf2d7ab76ee402f09a
Mateusz Gajdizk <mgajdzik@gmail.com>
2016-07-14 18:12:37 +0200

    cfd as separate function

--- src/js/ui/kanban-flow-ui.js
+++ src/js/ui/kanban-flow-ui.js
@@ -12,11 +12,10 @@ function GUI(hookSelectorParam, simulation, configuration) {
 	
 	this.stop = function() {
 		this.simulation.stop();
-		lastUpdatedCFDDay = -1;
+		this.cfdDiagram.redraw();
 		lastUpdatedLittlesDay = -1;
 		lastUpdatedCodDay = -1;
 		lastUpdatedScatterPlotDay = -1;
-		updateCFDConfiguration.bind(this)();
 		this.update(this.simulation.board, this.simulation.stats, true);
 	}
 	this.pause = function() {
@@ -148,95 +147,130 @@ function GUI(hookSelectorParam, simulation, configuration) {
 		$$(".backlog-settings-div").slideFadeToggle();
 	});
 
-	$$(".simulation-cfd").CanvasJSChart($.extend(true, {}, commonDiagramProperties, {
-	  toolTip: {
-		contentFormatter: function (e) {
-			var content = "Day: <strong>" + (e.entries[0].dataPoint.x + 1) + "</strong><br/>";
-			for (var i = e.entries.length - 1; i >= 0; i--) {
-				content += e.entries[i].dataSeries.name + ": <strong>" + e.entries[i].dataPoint.y + "</strong><br/>";
-			}
-			return content;
-		},
-	  },
-	  axisX:{
-		  labelFormatter : function(e) {
-			  return e.value + 1;
-		  }
-	  },
-	  axisY:{
-		  includeZero: false,
-	  },
-	  rangeChanged: function(e){
-		 var indexOfLowestElement = Math.floor(e.axisX.viewportMinimum);
-		 e.chart.options.axisY.minimum=e.chart.options.data[0].dataPoints[indexOfLowestElement].y;
-		 e.chart.render();
-	  },
-      data: [],
-    }));
-	$$(".simulation-cfd-tab").bind('isVisible', function() {
-		updateCFDConfiguration.bind(this)();
-		updateCFD(this.simulation.time, this.simulation.stats);
-	}.bind(this));
-	var colors = ['silver', 'mediumaquamarine', 'lightskyblue', 'lightpink', 'lightgray', 'lightcoral', 'lightblue', 'burlywood', 'antiquewhite'];
-	function updateCFDConfiguration() {
-		var groups = [];
-		var group = [];
-		var checkboxes = $$(".simulation-cfd-settings input[type='checkbox']");
-		for (var i=0; i<checkboxes.length; i++) {
-			var checkbox = checkboxes[i];
-			var checked = checkbox.checked;
-			if (checked) {
-				group = [];
-				groups.push(group);
-			}
-			group.push([checkbox.parentElement, this.simulation.board.columns[i]]);
-		}
-		for (var i=0; i<groups.length; i++) {
-			for (var j =0; j<groups[i].length; j++) {
-				$(groups[i][j][0]).css("backgroundColor", colors[i]);
-			}
-		}
-		var model = [];
-		for (var i=0; i<groups.length; i++) {
-			var columnsToSum = [];
-			var name = "group " + i;
-			var fromActiveColumn = false;
-			var fromColumn = false;
-			var fromCoumnsActive = [];
-			for (var j =0; j<groups[i].length; j++) {
-				var column = groups[i][j][1];
-				columnsToSum.push(column);
-				if (!fromColumn) {
-					name = column.label;
-					fromColumn = true;
+	function DiagramCFD(simulation) {
+		this.simulation = simulation;
+		$$(".simulation-cfd").CanvasJSChart($.extend(true, {}, commonDiagramProperties, {
+		  toolTip: {
+			contentFormatter: function (e) {
+				var content = "Day: <strong>" + (e.entries[0].dataPoint.x + 1) + "</strong><br/>";
+				for (var i = e.entries.length - 1; i >= 0; i--) {
+					content += e.entries[i].dataSeries.name + ": <strong>" + e.entries[i].dataPoint.y + "</strong><br/>";
 				}
-				if (!fromActiveColumn && !column.isQueue() && fromCoumnsActive.length == 0) {
-					name = column.label;
-					fromActiveColumn = true;
+				return content;
+			},
+		  },
+		  axisX:{
+			  labelFormatter : function(e) {
+				  return e.value + 1;
+			  }
+		  },
+		  axisY:{
+			  includeZero: false,
+		  },
+		  rangeChanged: function(e){
+			 var indexOfLowestElement = Math.floor(e.axisX.viewportMinimum);
+			 e.chart.options.axisY.minimum=e.chart.options.data[0].dataPoints[indexOfLowestElement].y;
+			 e.chart.render();
+		  },
+	      data: [],
+	    }));
+		$$(".simulation-cfd-tab").bind('isVisible', function() {
+			this.updateCFDConfiguration();
+			this.updateCFD();
+		}.bind(this));
+		var colors = ['silver', 'mediumaquamarine', 'lightskyblue', 'lightpink', 'lightgray', 'lightcoral', 'lightblue', 'burlywood', 'antiquewhite'];
+		this.updateCFDConfiguration = function() {
+			var groups = [];
+			var group = [];
+			var checkboxes = $$(".simulation-cfd-settings input[type='checkbox']");
+			for (var i=0; i<checkboxes.length; i++) {
+				var checkbox = checkboxes[i];
+				var checked = checkbox.checked;
+				if (checked) {
+					group = [];
+					groups.push(group);
 				}
-				if (!column.isQueue()) {
-					fromCoumnsActive.push(column);
+				group.push([checkbox.parentElement, this.simulation.board.columns[i]]);
+			}
+			for (var i=0; i<groups.length; i++) {
+				for (var j =0; j<groups[i].length; j++) {
+					$(groups[i][j][0]).css("backgroundColor", colors[i]);
 				}
-				if (fromCoumnsActive.length > 1) {
-					name = "";
-					for (var k=0; k<fromCoumnsActive.length; k++) {
-						name += fromCoumnsActive[k].shortLabel + " ";
+			}
+			var model = [];
+			for (var i=0; i<groups.length; i++) {
+				var columnsToSum = [];
+				var name = "group " + i;
+				var fromActiveColumn = false;
+				var fromColumn = false;
+				var fromCoumnsActive = [];
+				for (var j =0; j<groups[i].length; j++) {
+					var column = groups[i][j][1];
+					columnsToSum.push(column);
+					if (!fromColumn) {
+						name = column.label;
+						fromColumn = true;
+					}
+					if (!fromActiveColumn && !column.isQueue() && fromCoumnsActive.length == 0) {
+						name = column.label;
+						fromActiveColumn = true;
+					}
+					if (!column.isQueue()) {
+						fromCoumnsActive.push(column);
+					}
+					if (fromCoumnsActive.length > 1) {
+						name = "";
+						for (var k=0; k<fromCoumnsActive.length; k++) {
+							name += fromCoumnsActive[k].shortLabel + " ";
+						}
 					}
 				}
+				model[groups.length - 1 - i] = { type: "stackedArea", dataPoints: [], name: name.trim(), showInLegend: true, color: colors[i], columnsToSum: columnsToSum};
 			}
-			model[groups.length - 1 - i] = { type: "stackedArea", dataPoints: [], name: name.trim(), showInLegend: true, color: colors[i], columnsToSum: columnsToSum};
+			$$(".simulation-cfd").CanvasJSChart().options.data = model;
+			this.lastUpdatedCFDDay = -1;
+			this.updateCFD();
+		};
+		$$(".simulation-cfd-settings input[type='checkbox']").change(this.updateCFDConfiguration.bind(this));
+		$$(".simulation-cfd-settings div:not(:first-child)").click(function(event) {
+			var checkbox = $(event.target).find("input[type='checkbox']")[0];
+			if (!checkbox) return;
+			checkbox.checked = !checkbox.checked;
+			this.updateCFDConfiguration();
+		}.bind(this));
+		
+		this.lastUpdatedCFDDay = 0;
+		this.updateCFD = function() {
+			var time = this.simulation.time
+			var stats =  this.simulation.stats
+			var tab = $$(".simulation-cfd-tab:visible", false);
+			if (tab.length == 0) {
+				return;
+			}
+			var currentDay = Math.floor(time / (60 * 8));
+			if (currentDay <= this.lastUpdatedCFDDay) return;
+			this.lastUpdatedCFDDay = currentDay;
+			var model = $$(".simulation-cfd").CanvasJSChart().options.data;
+			for (var i=0; i<model.length; i++) {
+				var columnsToSum = model[i].columnsToSum;
+				for (var j=model[i].dataPoints.length; j<stats.cfdData[columnsToSum[0].name].length; j++) {
+					var sum = 0;
+					for (var k=0; k<columnsToSum.length; k++) {
+						sum += stats.cfdData[columnsToSum[k].name][j].y;
+					}
+					model[i].dataPoints[j] = {x: stats.cfdData[columnsToSum[0].name].x, y: sum};
+				}
+			}
+			$$(".simulation-cfd").CanvasJSChart().render();
 		}
-		$$(".simulation-cfd").CanvasJSChart().options.data = model;
-		lastUpdatedCFDDay = -1;
-		updateCFD(this.simulation.time, this.simulation.stats);
-	};
-	$$(".simulation-cfd-settings input[type='checkbox']").change(updateCFDConfiguration.bind(this));
-	$$(".simulation-cfd-settings div:not(:first-child)").click(function(event) {
-		var checkbox = $(event.target).find("input[type='checkbox']")[0];
-		if (!checkbox) return;
-		checkbox.checked = !checkbox.checked;
-		updateCFDConfiguration.bind(this)();
-	}.bind(this));
+		this.redraw = function() {
+			this.lastUpdatedCFDDay = -1;
+			this.updateCFDConfiguration();
+			this.updateCFD();
+		}
+	}
+	
+	this.cfdDiagram = new DiagramCFD(this.simulation);
 	
 	$$(".simulation-littles-tab").CanvasJSChart($.extend(true, {}, commonDiagramProperties, {
 	  axisX:{
@@ -415,7 +449,7 @@ function GUI(hookSelectorParam, simulation, configuration) {
 		updateTime(this.simulation.time, this.cache);
 		updateStats(stats, this.cache);
 		this.updateBoard();
-		updateCFD(this.simulation.time, stats);
+		this.cfdDiagram.updateCFD();
 		updateLittles(this.simulation.time, stats);
 		updateCod(this.simulation.time, stats, force);
 		updateScatterPlot(this.simulation.time, stats, force);
@@ -438,30 +472,6 @@ function GUI(hookSelectorParam, simulation, configuration) {
 		$$('.stats-wip-lead-time').text(wipAvg && leadTimeAvg ? (wipAvg / leadTimeAvg).toFixed(1) : '-');
 		$$('.stats-utilisation').text(stats.capacityUtilisation.getAvg() ? stats.capacityUtilisation.getAvg().toFixed(1) : '-');
 	}
-	
-	var lastUpdatedCFDDay = 0;
-	function updateCFD(time, stats) {
-		var tab = $$(".simulation-cfd-tab:visible", false);
-		if (tab.length == 0) {
-			return;
-		}
-		var currentDay = Math.floor(time / (60 * 8));
-		if (currentDay <= lastUpdatedCFDDay) return;
-		lastUpdatedCFDDay = currentDay;
-		var model = $$(".simulation-cfd").CanvasJSChart().options.data;
-		for (var i=0; i<model.length; i++) {
-			var columnsToSum = model[i].columnsToSum;
-			for (var j=model[i].dataPoints.length; j<stats.cfdData[columnsToSum[0].name].length; j++) {
-				var sum = 0;
-				for (var k=0; k<columnsToSum.length; k++) {
-					sum += stats.cfdData[columnsToSum[k].name][j].y;
-				}
-				model[i].dataPoints[j] = {x: stats.cfdData[columnsToSum[0].name].x, y: sum};
-			}
-		}
-		$$(".simulation-cfd").CanvasJSChart().render();
-	}
-	
 	
 	var lastUpdatedLittlesDay = 0;
 	function updateLittles(time, stats) {
