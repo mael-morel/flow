80ad49ae0979a0f646c725019b53675b96254c96
Mateusz Gajdizk <mgajdzik@gmail.com>
2016-07-14 17:42:26 +0200

    diagram settings separated

---
+++ src/js/ui/Diagram.js
@@ -0,0 +1,23 @@
+var commonDiagramProperties = {
+  backgroundColor: null,
+  zoomEnabled: true,
+  zoomType: "x",
+  axisY:{
+	  minimum: 0
+  },
+  axisY2:{
+	  minimum: 0,
+  },
+  axisX:{
+    minimum: 0,
+  },
+  toolTip: {
+ 	 shared: "true",
+  },
+  legend: {
+      horizontalAlign: "left",
+      verticalAlign: "top",
+      fontSize: 15,
+	  dockInsidePlotArea: true
+  },
+}
